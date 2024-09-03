31a2db50a8f4a651a2ac9c439e9cd42a4187275a
Radek Sláma <61729402+playmr365@users.noreply.github.com>
2024-09-03 17:12:06 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,23 +1,7 @@
 #!/bin/bash
-
-#formát hostnamu uvedeného ve zprávě
-HOSTNAME="$(hostname) ($(curl -4 icanhazip.com))"
-
-#Určení služeb k monitoringu běhu
-SERVICES=("haproxy" "httpd" "zabbix-agent2")
-
-#počet GB kdy začne posílat hlášky
-ALERT_DISK="5"
-#počet dní do expirace LE certu
-LE_EXPIRE="20"
-#množstvý ram v MB do posílání hlášek
-RAM_MIN="2048" # 2GB
-#Odkaz na ntfy server
-NTFY_SERVER=""
-
 #zapnutí jednotlivích částí monitoringu
-APP="true"
-RAM="true"
-CPU="true"
-DISK="true"
-LE="true"
+APP="true"  #Kontrola běhu aplikací
+RAM="true"  #Kontrola využití RAM
+CPU="true"  #Kontrola využití CPU
+DISK="true" #Kontrola obsazenosti diksu
+LE="true"   #Kontrola expirace LE certifikátu
