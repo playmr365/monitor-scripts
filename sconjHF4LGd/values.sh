f51d713a8b9716d805078800a4515e7964f08424
Radek Sláma <61729402+playmr365@users.noreply.github.com>
2024-09-03 17:14:49 +0200

    Update values.sh

--- sconjHF4LGd/values.sh
+++ sconjHF4LGd/values.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-HOSTNAME="$(hostname) ($(curl -4 icanhazip.com))"
-SERVICES=("haproxy" "httpd" "zabbix-agent2")
-ALERT_DISK="5"
-LE_EXPIRE="20"
-RAM_MIN="2048" # 2GB
-NTFY_SERVER="https://ntfy.slamaci.eu/servery"
+HOSTNAME="$(hostname) ($(curl -4 icanhazip.com))"   #Definice tvoření hostname
+SERVICES=("haproxy" "httpd" "zabbix-agent2")        #Definice služeb k monitoringu
+ALERT_DISK="5"                                      #Definice při kolika GB bude chodit alert
+LE_EXPIRE="20"                                      #Kolik dní do expirace certifikátu notifikovat
+RAM_MIN="2048"                                      #Při kolika Mb volné RAM notifikovat
+NTFY_SERVER="https://test.ntfy.cz/test"             #Na jakou URL volat notifikace do NTFY
