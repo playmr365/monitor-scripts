5ef3c66dc5d0659b1f1144102a369e1f14e052e1
Radek Sláma <61729402+playmr365@users.noreply.github.com>
2024-09-03 17:08:56 +0200

    Update ram.sh

--- sconjHF4LGd/ram.sh
+++ sconjHF4LGd/ram.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
-
-
+source /monitors/values.sh
 # Získání dostupné paměti v MB pomocí /proc/meminfo
 available_memory=$(awk '/MemAvailable/ {print $2 / 1024}' /proc/meminfo)
-
 # Porovnání s hranicí
 if (( $(echo "$available_memory > $RAM_MIN" | bc -l) )); then
     echo "ok"   
